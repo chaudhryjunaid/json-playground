edd308edf9099b23537a14ec1bb5d50cb635fc1d
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2018-12-29 14:48:01 +0500

    add index.js, decache and recache method

---
+++ index.js
@@ -0,0 +1,18 @@
+const decache = require('decache');
+const R = require('ramda');
+const _ = require('lodash');
+const moment = require('moment-timezone');
+const fs = require('fs');
+
+const reload = (module) => {
+	decache(module);
+	return require(module);
+};
+
+module.exports = {
+	R,
+	_,
+	moment,
+	fs,
+	reload
+};
